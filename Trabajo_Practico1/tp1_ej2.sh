a6d4a31a0c6964a1f5f541f3cff9b790c0b97d35
sntejada1 <sntejada1@gmail.com>
2019-09-03 23:12:26 -0300

    pequeño avance

--- Trabajo_Practico1/tp1_ej2.sh
+++ Trabajo_Practico1/tp1_ej2.sh
@@ -7,13 +7,16 @@ echo "Voy a trabajar en la direccion: $direc"
 elif [ $1 = "-h" ] ; then #Si el parametro enviado es consulta de ayuda muestro informacion
 echo "Voy a mostrar ayuda"
 #SALIR
+elif [ $1 = "--help" ] ; then #Si el parametro enviado es consulta de ayuda extendida muestro informacion
+echo "Voy a mostrar ayuda extendida"
+#SALIR
 elif [ -d "$1" ] ; then # valido si es una direccion
 direc=$1
 else 
     echo "El directorio ${direc} no existe"
 #SALIR
 fi 
-
+if [ -d "$direc" ] ; then
 echo "Voy a trabajar en la direccion: $direc"
 
 cd $direc
@@ -24,6 +27,6 @@ do
 echo "=$f="
 #mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr 'v' _)"; 
 done
-
+fi
 
 
