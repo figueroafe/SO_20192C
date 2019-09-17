942ff02c8214f8cf91afddd0e46fb894e7e3208b
serchyo <49600604+serchyo@users.noreply.github.com>
2019-09-17 10:39:49 -0300

    Update ej_6.sh

--- Trabajo_Practico1/tp1_ej6/ej_6.sh
+++ Trabajo_Practico1/tp1_ej6/ej_6.sh
@@ -66,11 +66,71 @@ else
 fi
 
 # Fin Verificacion de parametros
+path="$1"
+
+dirwsub=$(find "$path" -maxdepth 2 -type d)
 
 
+find /home/sergio/Documents -maxdepth 2 -type d | awk '/\.?\/Documents\/\w/'
+
+[0-9a-fA-F_-]
+
+find /home/sergio/Documents -maxdepth 2 -type d | awk '/\.?\/Documents\/[0-9a-fA-F]/'
+
+find /home/sergio/Documents -maxdepth 2 -type d | awk '/\.?\/Documents\/(?!\/)/'
+
+find /home/sergio/Documents -maxdepth 2 -type d | awk '/\.?\/Documents\/(?:(?!\/).)*/'
 
 
+^(?:(?!ab).)+$
 
+find /home/sergio/Documents -maxdepth 2 -type d | awk '/\.?\/Documents\/^(?:([\/]).)+$/'
 
 
 #########################################
+#! /bin/bash
+
+path="/home/sergio/Documents"
+
+readarray vect1 <<< "$(find "$path" -maxdepth 2 -mindepth 1  -type d)"
+
+readarray vect2 <<< "$(find "$path" -maxdepth 2 -mindepth 2  -type d)"
+
+vwsubd=()
+
+echo "${vect1[@]}"
+echo
+echo "${vect2[@]}"
+
+for i in "${vect1[@]}"; do
+
+    count=${#vect2[@]}
+
+    for j in "${vect2[@]}"; do    
+
+        echo "vect1"
+        echo "$i"
+        echo
+        echo "vect2"
+        echo "$j"
+        count=$(($count-1))
+
+        if [ "$i" != "$j" ];
+        then
+            echo "entro"
+            aux=("$i")
+        else
+            flag=1
+        fi
+    done
+
+    if [ $count -eq 0 && $flag -eq 0 ];
+    then
+        vwsubd+=("$aux")
+    fi
+
+    flag=0
+done
+
+echo "vector final"
+echo "${vwsubd[@]}"
