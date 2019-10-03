122d67a796ee9494307048a96144803a98caec82
Fernando Nicolas Sanchez <38439656+fns92@users.noreply.github.com>
2019-10-03 15:43:27 -0300

    Esqueleto Armado

--- Trabajo_Practico2/tp2_ej1/tp2_ej6.ps1
+++ Trabajo_Practico2/tp2_ej1/tp2_ej6.ps1
@@ -19,24 +19,41 @@
 
 Param
 (
-[parameter(Position = 1, Mandatory = $false)]
+[Parameter(Position = 1, Mandatory = $false)]
 [string] $Entrada = "vacio",
-[parameter(Position = 2, Mandatory = $false)]
-[string] $Producto = "vacio",
-[parameter(Position = 3, Mandatory = $false)]
-[int] $Suma
+[Parameter(Position = 2, Mandatory = $false<#, ParameterSetName = 'Producto'#>)]
+[int] $Producto = -999,
+[Parameter(Position = 3, Mandatory = $false<#, ParameterSetName = 'Suma'#>)]
+[string] $Suma = "vacio"
 )
 
+
 $path = Test-Path "$Entrada"
+$path2 = Test-Path "$Suma"
 if($Entrada -eq "vacio"){
     Write-Host "Imposible continuar - No se ingresó el origen de la matriz"
 }
 elseif ($path -ne $true) {
     Write-Host "Imposible continuar - El path no existe.."
 }
-elseif($Producto -eq "vacio"){
+elseif($Producto -eq -999 -and $Suma -eq "vacio"){
     Write-Host "Imposible continuar - No se ingresó una operacion"
 }
-elseif($Producto -lt 0){
-    Write-Host "El escalar es inválido, debe ser un número natural"
+elseif($Producto -ne -999) {
+    if($Producto -lt 0){
+        Write-Host "El escalar es inválido, debe ser un número natural"
+    }
+    else {
+        Write-Host "Realizando el producto, por favor espere..."
+        Write-Host "El Producto escalar se ha realizado exitosamente!"
+    }
+}
+elseif($Suma -ne "vacio"){
+    if($path2 -eq $true){
+        Write-Host "Realizando la suma, por favor espere..."
+        Write-Host "El Suma de Matrices se ha realizado exitosamente!"
+    }
+    else {
+        Write-Host "No se pudo ubicar el path de la matriz2"
+    }
 }
