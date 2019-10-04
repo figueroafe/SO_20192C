7db144f67d4189116b07b485c99586caa2258eec
figueroafe <figueroafe@gmail.com>
2019-10-04 16:55:31 -0300

    peleando con zipfile

--- Trabajo_Practico2/ej4/ej4.ps1
+++ Trabajo_Practico2/ej4/ej4.ps1
@@ -1,16 +1,14 @@
-
-		
 Param (
 	[Parameter(Position=0,Mandatory=$true)]
-	[string]$pathzip,
+	[string]$pathzip
 	
-	[Parameter(Position=1,Mandatory=$true)]
-	[string]$directorio,
+	#[Parameter(Position=1,Mandatory=$true)]
+	#[string]$directorio
 
-	[Parameter(Position=2,Mandatory=$true,ParameterSetName='Descomprimir')]
-	[Parameter(Position=2,Mandatory=$true,ParameterSetName='Comprimir')]
-	[Parameter(Position=2,Mandatory=$true,ParameterSetName='Informar')]
-	[switch]$comdesinf
+	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Descomprimir')]
+	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Comprimir')]
+	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Informar')]
+	#[switch]$comdesinf
 )
 
 	
@@ -21,11 +19,28 @@ Param (
 #Add-Type -AssemblyName 'system.io.compression.filesystem'
 #[System.IO.Compression.ZipFile]::CreateFromDirectory($pathzip, $directorio) 
 
-#Add-Type -Assembly 'System.IO.Compression.FileSystem'
-#[System.IO.Compression.ZipFile]::CreateFromDirectory($pathzip, $directorio)
+#if($comdesinf -eq 'Descomprimir')
+#{
+#	Add-Type -Assembly 'System.IO.Compression.FileSystem'
+#	[System.IO.Compression.ZipFile]::ExtractToDirectory($pathzip, $directorio)
+#
+#}
 
-Add-Type -Assembly 'System.IO.Compression.FileSystem'
-[System.IO.Compression.ZipFile]::ExtractToDirectory($pathzip, $directorio)
+
+
+#switch ($comdesinf) {
+#	"Descomprimir"	{ Add-Type -Assembly 'System.IO.Compression.FileSystem'
+#						[System.IO.Compression.ZipFile]::ExtractToDirectory($pathzip, $directorio); break}
+#	"Comprimir" 	{ Add-Type -Assembly 'System.IO.Compression.FileSystem'
+#						[System.IO.Compression.ZipFile]::CreateFromDirectory($pathzip, $directorio); break}
+#	"Informar"		{break}
+#	Default {"Opciones erroneas"; break}
+#}
+
+add-Type -AssemblyName system.io.compression.filesystem
+#[System.IO.Compression.ZipFile]::CreateFromDirectory($pathzip, $directorio)
+[io.compression.ZipFile]::OpenRead($pathzip)
+
 
 
 #if (-not (Test-Path $pathzip)) {
