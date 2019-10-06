397cf83de5f9476ae8f6d900afd2a1cd58deaf6e
Fernando Nicolas Sanchez <38439656+fns92@users.noreply.github.com>
2019-10-06 19:42:08 -0300

    70% Completado
    -Faltan preguntar cosas

--- Trabajo_Practico2/tp2_ej6/tp2_ej6.ps1
+++ Trabajo_Practico2/tp2_ej6/tp2_ej6.ps1
@@ -1,25 +1,3 @@
-
-#-----------------------Inicio Encabezado------------------------------##
-# Nombre Script: "tp2_ej6.ps1"
-# Numero Trabajo Practico: 2 
-# Numero Ejercicio: 6
-# Tipo: 1° Entrega
-# Integrantes:
-#
-#		Nombre y Apellido                                 DNI
-#		---------------------                           ----------
-#       Francisco Figueroa	                            32.905.374
-#       Adrian Morel		                            34.437.202
-#       Sergio Salas                                    32.090.753                 
-#       Fernando Sanchez	 		                	36.822.171
-#       Sabrina Tejada			       	     			37.790.024
-#
-##-----------------------Fin del Encabezado-----------------------------##
-
-
-
-
-
 <#
     .Synopsis
     Script creado para realizar el producto escalar de una matriz
@@ -99,6 +77,32 @@ elseif($Producto -ne -999) {
 elseif($Suma -ne "vacio"){
     if($path2 -eq $true){
         Write-Host "Realizando la suma, por favor espere..."
+        $Matriz1 = @(Get-Content $Entrada)
+        $filas = $Matriz1.count
+        $columnas = $Matriz1[0].Split("|").Length
+        $Matriz1 = @($Matriz1.Split("|")) 
+        $Matriz2 = @(Get-Content $Suma)
+        $Matriz2 = @($Matriz2.Split("|")) 
+
+        Remove-Item salida.matriz1.txt
+        New-Item salida.matriz1.txt | Out-Null
+        $pos = 0;
+        for ($i = 0; $i -lt $filas; $i++) {
+            
+            for ($j = 0; $j -lt $columnas; $j++) {
+                [int]$res = [int]$Matriz1[$pos] + [int]$Matriz2[$pos] 
+                 if($j -eq $columnas-1){
+                    $array += "$res"
+                 }
+                 else {
+                    $array += "$res|"
+                 }
+                 $pos++;                 
+             }
+             Write-Host $array
+             Write-Output $array >> salida.matriz1.txt
+             $array = ""
+        }
         Write-Host "El Suma de Matrices se ha realizado exitosamente!"
     }
     else {
