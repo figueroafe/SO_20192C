11908f9728f760740f9a9e7551fe1fbb399ae56b
figueroafe <figueroafe@gmail.com>
2019-10-06 18:37:30 -0300

    Agregue opcion informar

--- Trabajo_Practico2/ej4/ej4.ps1
+++ Trabajo_Practico2/ej4/ej4.ps1
@@ -1,17 +1,26 @@
 Param (
 	[Parameter(Position=0,Mandatory=$true)]
-	[string]$pathzip
+	[string]$pathzip,
 	
-	#[Parameter(Position=1,Mandatory=$true)]
-	#[string]$directorio
-
+	[Parameter(Mandatory=$false)]
+	[string]$directorio
+	
+	#[Parameter(Mandatory=$true,ParameterSetName='Descomprimir')]
+	#[Parameter(Mandatory=$true,ParameterSetName='Comprimir')]
+	#[Parameter(Mandatory=$true,ParameterSetName='Informar')]
+	#[switch]$comdesinf
+	
 	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Descomprimir')]
 	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Comprimir')]
 	#[Parameter(Position=2,Mandatory=$true,ParameterSetName='Informar')]
 	#[switch]$comdesinf
 )
 
-	
+$file_exits = Test-Path $pathzip;
+if($file_exits -ne $true){
+	Write-Host "El archivo de entrada no existe. Ultice la opcion Get-Help para ver la ayuda del script."
+	exit;
+}
 
 	
 #Write-Host "Mostrar $pathzip y $directorio"
@@ -38,11 +47,45 @@ Param (
 #}
 
 add-Type -AssemblyName system.io.compression.filesystem
-#[System.IO.Compression.ZipFile]::CreateFromDirectory($pathzip, $directorio)
-[io.compression.ZipFile]::OpenRead($pathzip)
+try{
+	$RawZips = [io.compression.ZipFile]::OpenRead($pathzip).Entries
+}catch{
+	Write-Host "El archivo de entrada no existe. Ultice la opcion Get-Help para ver la ayuda del script."
+	exit;
+}	
+
+$ObjArray = @();
+foreach($RawZip in $RawZips){
+	$comprimido=($RawZip.CompressedLength/1KB).ToString(00);
+	$original=00;
+	if($RawZip.Length -gt 0){
+		$original=($RawZip.Length/1KB).ToString(00);
+	}
+	
+	$object = New-Object psobject
+	$object | Add-Member -MemberType NoteProperty -Name Nombre_Archivo($RawZip.Name)
+	$orig=($original/1024).ToString()+" MB";
+	$object | Add-Member -MemberType NoteProperty -Name Tamaño_Original($orig)
+	$compri=($comprimido/1024).ToString()+" MB",
+	$object | Add-Member -MemberType NoteProperty -Name Tamaño_Comprimido($compri)
+
+	#valido que no se divida por cero
+	$notzero=0;
+	try{
+		$notzero=$comprimido/$original;
+	}catch{
+		$divido_cero=1;
+	}
+	#Muestro la relación con 2 decimales
+	$object | Add-Member NoteProperty Relación($notzero.ToString("0.00"))
+	if($RawZip.Name -and $original){
+		$ObjArray += $object
+	}
+}
+
+#Imprimo
+$ObjArray | Format-Table
 
 
 
-#if (-not (Test-Path $pathzip)) {
-#	"Directorio Inexistente."
-#}
+
