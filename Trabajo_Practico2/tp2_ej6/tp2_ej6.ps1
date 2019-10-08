1ef6b001a0e56a58f34178eaa9c5701ffcfd3075
Fernando Nicolas Sanchez <38439656+fns92@users.noreply.github.com>
2019-10-08 11:41:32 -0300

    Funcionalmente Terminado.

--- Trabajo_Practico2/tp2_ej6/tp2_ej6.ps1
+++ Trabajo_Practico2/tp2_ej6/tp2_ej6.ps1
@@ -3,7 +3,7 @@
     Script creado para realizar el producto escalar de una matriz
     o la suma entre 2 matrices.
     .Description
-    El Script se utiliza para realizar la suma  de 
+    El Script se utiliza para realizar la suma de 
     2 matrices pasadas por parámetro en archivos de texto; devolviendo el resultado
     en un tercer archivo de texto. También puede calcular el producto escalar de
     una matriz pasando un escalar por parámetro.
@@ -28,12 +28,91 @@ Param
 [string] $Suma = "vacio"
 )
 
-Write-Host "Procesando..."
-for ($i = 0; $i -lt 5; $i++) {
-    Start-Sleep -Seconds 0.2
-    Write-Host("#")
+function procesar(){
+    for ($i = 0; $i -lt 3; $i++) {
+        Start-Sleep -Seconds 0.2
+        Write-Host("#")
+        
+    }
+}
+
+function ProductoEscalar(){
+    Write-Host "Realizando el producto, por favor espere..."
+    procesar
+    $Matriz1 = @(Get-Content $Entrada)
+    $filas = $Matriz1.count
+    $columnas = $Matriz1[0].Split("|").Length
+    $Matriz1 = @($Matriz1.Split("|")) 
+  #  $array = @()#New-Object System.Collections.ArrayList
+    $salida = Test-Path "salida.$NombreArchivo"
+    if($salida -eq $true){
+        Remove-Item "salida.$NombreArchivo" | Out-Null
+        New-Item "salida.$NombreArchivo" | Out-Null
+    }
+    else {
+        New-Item "salida.$NombreArchivo" | Out-Null
+    }
+    
+    $pos = 0;
+    for ($i = 0; $i -lt $filas; $i++) {
+        
+        for ($j = 0; $j -lt $columnas; $j++) {
+            [float]$res = [float]$Matriz1[$pos] * $Producto 
+             #$array.Add($res)
+             if($j -eq $columnas-1){
+                $array += "$res"
+             }
+             else {
+                $array += "$res|"
+             }
+             $pos++;                 
+         }
+         Write-Output $array >> "salida.$NombreArchivo"
+         $array = ""
+    }
+    
+    Write-Host "El Producto escalar se ha realizado exitosamente!"
     
 }
+
+function SumarMatrices(){
+    Write-Host "Realizando la suma, por favor espere..."
+    procesar
+    $Matriz1 = @(Get-Content $Entrada)
+    $filas = $Matriz1.count
+    $columnas = $Matriz1[0].Split("|").Length
+    $Matriz1 = @($Matriz1.Split("|")) 
+    $Matriz2 = @(Get-Content $Suma)
+    $Matriz2 = @($Matriz2.Split("|")) 
+    $salida = Test-Path "salida.$NombreArchivo"
+    if($salida -eq $true){
+        Remove-Item "salida.$NombreArchivo" | Out-Null
+        New-Item "salida.$NombreArchivo" | Out-Null
+    }
+    else {
+        New-Item "salida.$NombreArchivo" | Out-Null
+    }
+    $pos = 0;
+    for ($i = 0; $i -lt $filas; $i++) {
+        
+        for ($j = 0; $j -lt $columnas; $j++) {
+            [float]$res = [float]$Matriz1[$pos] + [float]$Matriz2[$pos] 
+             if($j -eq $columnas-1){
+                $array += "$res"
+             }
+             else {
+                $array += "$res|"
+             }
+             $pos++;                 
+         }
+         Write-Output $array >> "salida.$NombreArchivo"
+         $array = ""
+    }
+    Write-Host "El Suma de Matrices se ha realizado exitosamente!"
+}
+
+Write-Host "Procesando..."
+procesar
 $path = Test-Path "$Entrada"
 $path2 = Test-Path "$Suma"
 $NombreArchivo = $Entrada.Substring($Entrada.LastIndexOf("/")+1)
@@ -51,80 +130,14 @@ elseif($Producto -ne -999) {
         Write-Host "El escalar es inválido, debe ser un número natural"
     }
     else {
-        Write-Host "Realizando el producto, por favor espere..."
-        $Matriz1 = @(Get-Content $Entrada)
-        $filas = $Matriz1.count
-        $columnas = $Matriz1[0].Split("|").Length
-        $Matriz1 = @($Matriz1.Split("|")) 
-      #  $array = @()#New-Object System.Collections.ArrayList
-        $salida = Test-Path "salida.$NombreArchivo"
-        if($salida -eq $true){
-            Remove-Item "salida.$NombreArchivo" | Out-Null
-            New-Item "salida.$NombreArchivo" | Out-Null
-        }
-        else {
-            New-Item "salida.$NombreArchivo" | Out-Null
-        }
-        
-        $pos = 0;
-        for ($i = 0; $i -lt $filas; $i++) {
-            
-            for ($j = 0; $j -lt $columnas; $j++) {
-                [float]$res = [float]$Matriz1[$pos] * $Producto 
-                 #$array.Add($res)
-                 if($j -eq $columnas-1){
-                    $array += "$res"
-                 }
-                 else {
-                    $array += "$res|"
-                 }
-                 $pos++;                 
-             }
-             Write-Host $array
-             Write-Output $array >> "salida.$NombreArchivo"
-             $array = ""
-        }
-        
-        Write-Host "El Producto escalar se ha realizado exitosamente!"
+        ProductoEscalar
     }
 }
 elseif($Suma -ne "vacio"){
     if($path2 -eq $true){
-        Write-Host "Realizando la suma, por favor espere..."
-        $Matriz1 = @(Get-Content $Entrada)
-        $filas = $Matriz1.count
-        $columnas = $Matriz1[0].Split("|").Length
-        $Matriz1 = @($Matriz1.Split("|")) 
-        $Matriz2 = @(Get-Content $Suma)
-        $Matriz2 = @($Matriz2.Split("|")) 
-        $salida = Test-Path "salida.$NombreArchivo"
-        if($salida -eq $true){
-            Remove-Item "salida.$NombreArchivo" | Out-Null
-            New-Item "salida.$NombreArchivo" | Out-Null
-        }
-        else {
-            New-Item "salida.$NombreArchivo" | Out-Null
-        }
-        $pos = 0;
-        for ($i = 0; $i -lt $filas; $i++) {
-            
-            for ($j = 0; $j -lt $columnas; $j++) {
-                [float]$res = [float]$Matriz1[$pos] + [float]$Matriz2[$pos] 
-                 if($j -eq $columnas-1){
-                    $array += "$res"
-                 }
-                 else {
-                    $array += "$res|"
-                 }
-                 $pos++;                 
-             }
-             Write-Host $array
-             Write-Output $array >> "salida.$NombreArchivo"
-             $array = ""
-        }
-        Write-Host "El Suma de Matrices se ha realizado exitosamente!"
+        SumarMatrices
     }
     else {
-        Write-Host "No se pudo ubicar el path de la matriz2"
+        Write-Host "Imposible continuar - No se pudo ubicar el path de la matriz2"
     }
 }
