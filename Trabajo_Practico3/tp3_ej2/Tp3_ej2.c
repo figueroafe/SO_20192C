3e3fcbe8ac2cac3148b10cd21e8154f387e4965c
sntejada1 <sntejada1@gmail.com>
2019-11-16 12:48:53 -0300

    final

--- Trabajo_Practico3/tp3_ej2/Tp3_ej2.c
+++ Trabajo_Practico3/tp3_ej2/Tp3_ej2.c
@@ -4,7 +4,26 @@
 #include <string.h>
 #include <math.h>
 #define TAM 50
-    pthread_mutex_t mutex;
+pthread_mutex_t mutex;
+
+/************  inicio encabezado  **************
+
+# Nombre Script: "Tp3_ej2.sh"
+# Numero Trabajo Practico: 3
+# Numero Ejercicio: 2
+# Tipo: 1° Entrega
+# Integrantes:
+#
+#		Nombre y Apellido                           DNI
+#		---------------------                       ----------
+#       Francisco Figueroa	                        32.905.374
+#       Adrian Morel		                        34.437.202
+#       Sergio Salas                                32.090.753                 
+#       Fernando Sanchez	 		                36.822.171
+#       Sabrina Tejada			       	     		37.790.024
+
+***************  fin encabezado  ***************/
+
 
 typedef struct{
     int tamVec, cantS, ubic;
@@ -26,12 +45,14 @@ void mostrar_ayuda()
 	printf("La suma se realizara sobre una cantidad de threads ingresadas por parametro. \n");
 	printf("\n\nSintaxis:\n");
 	printf("En la ubicacion del makefile ejecutar\n");
-	printf("make Tp3_ej2 para compilar");
-	printf("./Tp3_ej2[cantidad de threads] [directorio donde se almacenan los archivos a procesar] \n");
-    printf("make Tp3_ej2 [-help/-?] \n");
+	printf("make Tp3_ej2 para compilar\n");
+	printf("\nPara ejecutar:\n");
+	printf("./Tp3_ej2 [directorio donde se almacenan los archivos a procesar][cantidad de threads]\n");
+    printf("./Tp3_ej2 [-help/-?] \n");
 	printf("\n\nEjemplo:\n");
 	printf("./Tp3_ej2 -? \n");
-	printf("./Tp3_ej2 -? \n");
+	printf("./Tp3_ej2 ~/Documentos/Ej2Tp3/Vectores.txt  \n");
+	printf("./Tp3_ej2");
 }
 void *sumarVectores (void *arg){
 
@@ -56,35 +77,94 @@ void *sumarVectores (void *arg){
     return;
 }
 
-int main()
+int redondeo(double a){
+    int p_entera= (int)a;
+    if((a-p_entera)!= 0){
+
+        return (int)a+1;
+    }
+     return (int)a;
+}
+
+
+int main(int argc, char *argv[])
 {
     int canth, ok =0, ch;
     FILE *arch, *salida;
     char linea[TAM],n1[TAM],n2[TAM], ruta[50];
     t_params p;
 
-    printf("ingrese ruta de archivo con vectores (sin .txt):\n");
-    scanf("%s",&ruta);
-    fflush(stdout);
-    strcat(ruta,".txt");
-    ///validar rutas absolutas y relativas
-    arch = fopen(ruta,"r");
-    if(!arch){
-        printf("No existe archivo\n");
-        return 1;
-    }
-    do{
-        printf("ingrese entero de cantidad de hilos a usar:\n");
-        fflush(stdout);
-        if((ok = scanf("%d",&canth)) ==EOF)
-            return 1;
-        if(canth <= 0)
-        {
-            printf("La cantidad de hilos ingresada debe ser entera positiva mayor a cero. \n");
-            return 1;
+    /**VALIDACIONES**/
+    if(argc==3 || argc==2 ||argc==1){
+        if(argc==2 && (!strcmp(argv[1],"-help") || !strcmp(argv[1],"-?"))){
+            mostrar_ayuda();
+            exit(1);
+        }else{
+            if(argc==2){
+            /**validar direcccion y se pide cantidad*/
+                if(!(arch = fopen(argv[1],"r"))){
+                    printf("No existe archivo\n");
+                    return 1;
+                }
+                do{
+                    printf("ingrese entero de cantidad de hilos a usar:\n");
+                    fflush(stdout);
+                    if((ok = scanf("%d",&canth)) ==EOF)
+                        return 1;
+                    if(canth <= 0)
+                    {
+                        printf("La cantidad de hilos ingresada debe ser entera positiva mayor a cero. \n");
+                        return 1;
+                    }
+                    while((ch = getchar()) != EOF && ch != '\n');
+                }while(!ok);
+
+            }
+
         }
-        while((ch = getchar()) != EOF && ch != '\n');
-    }while(!ok);
+        if(argc==3){
+            /** validar dir y nro**/
+            if(!(arch = fopen(argv[1],"r"))){
+                printf("No existe archivo\n");
+                return 1;
+            }
+            canth=atoi(argv[2]);
+            if( canth <=0 || canth >100){
+                printf("Cantidad Invalida\n");
+                return 1;
+             }
+
+            }
+        if(argc==1){
+            printf("ingrese ruta de archivo con vectores (sin .txt):\n");
+            scanf("%s",&ruta);
+            fflush(stdout);
+            strcat(ruta,".txt");
+            if(!(arch = fopen(ruta,"r"))){
+                printf("No existe archivo\n");
+                return 1;
+
+            }
+            do{
+                printf("ingrese entero de cantidad de hilos a usar:\n");
+                fflush(stdout);
+                if((ok = scanf("%d",&canth)) ==EOF)
+                    return 1;
+                if(canth <= 0)
+                {
+                    printf("La cantidad de hilos ingresada debe ser entera positiva mayor a cero. \n");
+                    return 1;
+                }
+                while((ch = getchar()) != EOF && ch != '\n');
+            }while(!ok);
+        }
+
+    }else{
+        printf("La cantidad de parámetros no es la correcta %d\n", argc);
+
+        exit(1);
+    }
+
 
     ///Leer 2 vectores
     p.tamVec =0;
@@ -114,14 +194,18 @@ int main()
     fclose(arch);
     if(p.tamVec == 0){
         printf("El archivo se encuentra vacio \n");
+        salida = fopen("Salida.txt","wt");
+        fprintf(salida,"El archivo origen se encuentra vacio" );
+        fclose(salida);
         return 1;
     }
 
-    printf("Cantidad de elementos a sumar: %d\n", p.tamVec+1);
+    printf("Cantidad de elementos a sumar: %d\n", p.tamVec);
 
     ///repartir trabajo en los hilos
     p.ubic=0;
-    p.cantS = ceil((p.tamVec+1)/canth);
+    p.cantS = redondeo(((double)p.tamVec)/(double)canth);
+
     printf("cant maxima de sumas x hilo = %d \n",p.cantS );
     pthread_t h[canth];
     pthread_mutex_init(&mutex, NULL);
