bad06f69d210cae3c932df481d5f16b6766e47d9
Fernando Nicolas Sanchez <38439656+fns92@users.noreply.github.com>
2019-11-19 18:38:08 -0300

    Entregable ejercicio 4

---
+++ Trabajo_Practico3/tp3_ej4/cliente.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/ipc.h>
+
+void ayuda(void);
+sem_t* mutex_0;
+sem_t* mutex_1;
+sem_t* mutex_2;
+char *buffer;
+int fd;
+
+int main(int argc, char *argv[])
+{
+    //VALIDACIONES
+    if(argc != 2){
+        printf("\nCantidad de parámetros incorrectos, consulte ayuda con %s -help.\n\n", argv[0]);
+        return 1;
+    }
+
+    else{
+
+        if(strcmp(argv[1],"-help") == 0){
+            ayuda();
+            return 1;
+        }
+
+    }
+    //BUSCA EL ESPACIO DE MEMORIA COMPARTIDA
+    fd = shm_open("shm", O_RDWR, 0777);
+    mutex_0 = sem_open("mutex_0", O_RDWR);
+    mutex_1 = sem_open("mutex_1", O_RDWR);
+    mutex_2 = sem_open("mutex_2", O_RDWR);
+    //MAPEO DEL ESPACIO DE MEMORIA COMPARTIDA
+
+    buffer = (char *)mmap(0, 65536, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("Pidiendo semaforo de share memory...\n");
+    sem_wait(mutex_1);
+    printf("Semaforo otorgado...Acesso a share memory...\n");
+    strcpy(buffer, argv[1]);
+    printf("Liberando semaforo de lectura del buffer...\n");
+    sem_post(mutex_0);
+    printf("Semaforo liberado!...Pidiendo semaforo de respuesta..\n");
+    sem_wait(mutex_2);
+    printf("Semaforo de respuesta obtenido....\n");
+    puts(buffer);
+    printf("Liberando share memory para la siguiente consulta...\n");
+    sem_post(mutex_1);
+    printf("Finalizado!\n");
+    sem_close(mutex_0);
+    sem_close(mutex_1);
+    sem_close(mutex_2);
+    munmap(buffer, 65536);
+    return 1;
+}
+    //FUNCIONES
+void ayuda(){
+
+    printf("\n-------------------------------------------------------------------------------\n");
+    printf("--------------------------- Ayuda Ejercicio 4 ---------------------------------\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\nDescripción\n");
+    printf("El proceso lee de un espacio de memoria compartida las consultas y\n");
+    printf("devuelve por pantalla los registros coincidentes que encuentre. Toma\n");
+    printf("por parámetro la consulta con formato CAMPO=VALOR.\n");
+    printf("\nParámetros\n");
+    printf("Consulta: Consulta a realizar.\n");
+    printf("\nSintáxis\n");
+    printf("./tp3_ej4 [consulta]\n");
+    printf("\nEjemplos\n");
+    printf("./tp3_ej4 MARCA=GEORGALOS\n\n");
+}
---
+++ Trabajo_Practico3/tp3_ej4/servidor.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <time.h>
+
+sem_t* mutex_0, *mutex_1, *mutex_2;
+int fd;
+char *buffer;
+void ayuda(void);
+
+int main(int argc, char *argv[])
+{
+      if(argc > 2 || argc <=1 ){
+	printf("La cantidad de parametros no es correcta, utilice ./servidor -help para ver la ayuda..\n");
+	return 1;
+      }
+      
+      //Validaciones
+      if(argc == 2 && strcmp(argv[1],"-help") == 0)
+      {
+         ayuda();
+         return 1;
+      }
+
+    //Fin validaciones
+
+    char *aux='\0';
+    char id_item[100];
+    char articulo[100];
+    char producto[100];
+    char marca[100];
+    char campo[100];
+    char valor[100];
+    char linea[500];
+    FILE *fp;
+    int contador=0, consultas = 0;
+
+    fd = shm_open("shm", O_CREAT | O_EXCL | O_RDWR, 0600); //creacion de memoria compartida
+    ftruncate(fd, 65536);
+	mutex_0 = sem_open("mutex_0", O_CREAT, 0777, 0); //creacion de semaforo
+	mutex_1 = sem_open("mutex_1", O_CREAT, 0777, 1); //creacion de semaforo
+	mutex_2 = sem_open("mutex_2", O_CREAT, 0777, 0); //creacion de semaforo
+
+while(1  && consultas < 5){
+
+    printf("Pidiendo semaforo de lectura...\n");
+    sem_wait(mutex_0);
+    printf("Semaforo obtenido, leyendo consulta...\n");
+    buffer = (char *) mmap(0, 65536, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("\nconsulta recibida: %s\n\n", buffer);
+    aux=strrchr(buffer, '=');
+    if(aux == NULL){
+        strcpy(buffer, "\nRevise la consulta, debe tener formato CAMPO=VALOR\n");
+	sem_post(mutex_2);
+	sem_close(mutex_0);
+    	sem_close(mutex_1);
+    	sem_close(mutex_2);
+    	munmap(buffer, 65536);
+    	shm_unlink("shm");
+        return 1;
+    }
+
+    aux = strtok(aux, "\n");
+    strcpy(valor, aux+1);
+    *aux = '\0';
+
+    aux = strtok(buffer,"=");
+    strcpy(campo, aux);
+    *aux = '\0';
+
+    fp = fopen(argv[1], "r");
+    if(fp == NULL){
+        printf("\nNo se encontró el archivo de artículos, revise la ubicación.\n");
+	printf("Eliminando segmento de memoria compartida...\n");
+        sem_close(mutex_0);
+    	sem_close(mutex_1);
+    	sem_close(mutex_2);
+    	munmap(buffer, 65536);
+    	shm_unlink("shm");
+        return 1;
+    }
+
+    buffer[0] = '\0';
+    printf("Buscando coincidencias...\n");
+    sleep(10);
+    while(fgets(linea, sizeof(linea), fp)){
+
+        aux = strrchr(linea, '\r');
+        if(aux != NULL){
+            aux=strtok(strrchr(linea, ';'), "\r");
+            strcpy(marca, aux+1);
+            *aux = '\0';
+        }
+
+        else{
+            aux=strtok(strrchr(linea, ';'), "\n");
+            strcpy(marca, aux+1);
+            *aux = '\0';
+        }
+
+        strcpy(marca, aux+1);
+        *aux = '\0';
+
+        aux = strrchr(linea, ';');
+        strcpy(producto, aux+1);
+        *aux = '\0';
+
+        aux = strrchr(linea, ';');
+        strcpy(articulo, aux+1);
+        *aux = '\0';
+
+        sscanf(linea, "%10s", id_item);
+
+        if((strcmp(id_item, valor) == 0 && strcmp("ITEM_ID", campo) == 0) ||
+            (strcmp(articulo, valor) == 0 && strcmp("ARTICULO", campo) == 0) ||
+            (strcmp(producto, valor) == 0 && strcmp("PRODUCTO", campo) == 0) ||
+            (strcmp(marca, valor) == 0 && strcmp("MARCA", campo) == 0)){
+
+            contador++;
+
+            strcat(buffer, id_item);
+            strcat(buffer, ";");
+            strcat(buffer, articulo);
+            strcat(buffer, ";");
+            strcat(buffer, producto);
+            strcat(buffer, ";");
+            strcat(buffer, marca);
+            strcat(buffer, "\n");
+
+        }
+
+    }
+	
+	if(contador == 0)
+   	strcpy(buffer, "\nNo se encontraron registros que coincidan con la búsqueda.\n\n");
+    rewind(fp);    
+    printf("Enviando respuesta...\n");
+    sem_post(mutex_2);
+    printf("Semaforo de lectura de buffer liberado...\n");
+    consultas++;
+}
+    fclose(fp);
+    printf("Eliminando segmento de memoria compartida...\n");
+    sem_close(mutex_0);
+    sem_close(mutex_1);
+    sem_close(mutex_2);
+    munmap(buffer, 65536);
+    shm_unlink("shm");
+    return(1);
+}
+
+//FUNCIONES
+void ayuda(){
+
+    printf("\n-------------------------------------------------------------------------------\n");
+    printf("--------------------------- Ayuda Ejercicio 4 ---------------------------------\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n*Descripción:\n\n");
+    printf("El Servidor crea un espacio de memoria compartida en el cual los\n");
+    printf("procesos consumidores pueden generar consultas sobre el fichero maestro.\n");
+    printf("El espacio de memoria compartida esta sincronizado mediante el uso de\n");
+    printf("semaforos. Requiere como parametro la ruta del fichero maestro.\n");
+    printf("Las respuestas tienen una demora de 10seg para que se pueda ver el uso\n");
+    printf("sincronizado de los consumidores al area critica para consultar.\n");
+    printf("El segmento se elimina al ingresar una consulta erronea o al realizar 5 \n");
+    printf("consultas, solo para los fines practicos del ejercicio del tp.\n\n");
+    printf("*Ejemplos\n\n");
+    printf("./server /tmp/articulos.txt \n\n");
+}
+
