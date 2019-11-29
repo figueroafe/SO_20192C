3dcd70061f600b8093d8955252454e5b39101780
Fernando Nicolas Sanchez <38439656+fns92@users.noreply.github.com>
2019-11-29 16:00:32 -0300

    Ya casi - Probarlo

--- Trabajo_Practico3/tp3_ej1/ej1.c
+++ Trabajo_Practico3/tp3_ej1/ej1.c
@@ -1,3 +1,19 @@
+/************  inicio encabezado  **************
+# Nombre Script: "tp3_ej1.c"
+# Numero Trabajo Practico: 3
+# Numero Ejercicio: 1
+# Tipo 2° Entrega
+# Integrantes:
+#
+#		Nombre y Apellido                           DNI
+#		---------------------                       ----------
+#       Francisco Figueroa	                        32.905.374
+#       Adrian Morel		                        34.437.202
+#       Sergio Salas                                32.090.753
+#       Fernando Sanchez	 		                36.822.171
+#       Sabrina Tejada			       	     		37.790.024
+***************  fin encabezado  ***************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,13 +21,32 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-pid_t pid, pid2, pid3, pid4, pid5, pid6, pid7, pid8, pid9, pid10;
+pid_t pid, pid2, pid3, pid4, pid5, pid6, pid7, pid8, pid9, pid10, sid;
 //int padre = 0;
 void crearHijos();
 void crearDemonio();
+void ayuda();
 
-int main(){
+int main(int argc, char *argv[]){
+
+        if(argc > 2){
+        printf("\nCantidad de parámetros incorrectos, consulte ayuda con %s -help.\n\n", argv[0]);
+        return 1;
+    }
+
+    if(argc == 2){
+
+        if(strcmp(argv[1],"-help") == 0) {
+            ayuda();
+            return 1;
+        }
+        else{
+            printf("\nNo se reconoce parámetro, consulte ayuda con %s -help.\n\n", argv[0]);
+            return 1;
+        }
+    }
 
     crearHijos();
     getchar();
@@ -60,7 +95,7 @@ void crearHijos(){
                     printf ("PID: %d PPID: %d Parentesco-Tipo: [bisnieto] - [demonio]\n", getpid(), getppid());
                     crearDemonio();
                     }
-                    else //soy nieto                 
+                   else //soy nieto                 
                     waitpid(pid7, NULL, 0);
                     waitpid(pid8, NULL, 0);
                     }
@@ -101,5 +136,27 @@ void crearHijos(){
 }
 
 void crearDemonio(){
+  umask(0);
+  sid = setsid();
  while(1){}   
+}
+
+void ayuda(){
+
+    printf("\n--------------------------------------------------------------------------------\n");
+    printf("--------------------------- Ayuda Ejercicio 1 ----------------------------------\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("\nDescripción\n");
+    printf("El script genera mediante el uso de fork() el siguiente escenario:\n");
+    printf("\n");
+    printf("2 procesos hijos\n");
+    printf("3 procesos nietos\n");
+    printf("5 procesos bisnietos\n");
+    printf("2 procesos zombies (2 bisnietos)\n");
+    printf("3 procesos demonios (1 nieto y 2 bisnietos)\n");
+    printf("\nParámetros\n");
+    printf("No tiene.\n");
+    printf("\nEjemplos\n");
+    printf("./tp3_ej1\n\n");
+
 }
